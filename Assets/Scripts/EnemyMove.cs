1d6dc2042516ffb9bce8808569b7df2a01966531
varlogen <martynovpavel8@ya.ru>
2019-03-24 17:01:27 +0300

    enemy spawn and simple move

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    private Transform spaceBase;
+    private float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spaceBase = GameObject.FindWithTag("Base").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, spaceBase.position, Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public int enemyCount;
+    public GameObject enemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < enemyCount; i++)
+        {
+            GameObject respawns = GameObject.FindGameObjectWithTag("EnemySpawnArea");
+            float randX = Random.Range(-GetComponent<BoxCollider2D>().bounds.size.x / 2, GetComponent<BoxCollider2D>().bounds.size.x / 2);
+            float randY = transform.position.y + Random.Range(-GetComponent<BoxCollider2D>().bounds.size.y / 2, GetComponent<BoxCollider2D>().bounds.size.y / 2);
+            Vector2 spavnEnemyPosition = new Vector2(randX, randY);
+            Instantiate(enemyPrefab, spavnEnemyPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
