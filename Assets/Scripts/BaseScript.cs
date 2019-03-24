be4a3e569eed3e5e9f84e5f6fc07dba4fb69a082
TimurSirkin <timur_sirkin@mail.ru>
2019-03-24 20:24:02 +0300

    Swap scenes

--- Assets/Scripts/BaseScript.cs
+++ Assets/Scripts/BaseScript.cs
@@ -5,16 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class BaseScript : MonoBehaviour
 {
-    //private Rigidbody2D rb;
-
     void Start()
     {
-        //rb = GetComponent<Rigidbody2D>();
-        //rb.AddTorque(2f);
     }
 
     private void OnDestroy()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Menu");
     }
 }
