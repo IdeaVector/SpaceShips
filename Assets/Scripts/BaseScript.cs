8bcffead164967db0f5dd710ad9cdba891bec6a0
TimurSirkin <timur_sirkin@mail.ru>
2019-03-24 20:05:01 +0300

    BaseScript changed

--- Assets/Scripts/BaseScript.cs
+++ Assets/Scripts/BaseScript.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class BaseScript : MonoBehaviour
 {
+    //private Rigidbody2D rb;
+
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+        //rb.AddTorque(2f);
+    }
+
     private void OnDestroy()
     {
         SceneManager.LoadScene(1);
