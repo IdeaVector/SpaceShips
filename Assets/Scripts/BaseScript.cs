49770e0b21b229b783248578afe4e8d61b429fa9
TimurSirkin <timur_sirkin@mail.ru>
2019-03-24 19:33:13 +0300

    Add rout to menu after defeat

---
+++ Assets/Scripts/BaseScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BaseScript : MonoBehaviour
+{
+    private void OnDestroy()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
