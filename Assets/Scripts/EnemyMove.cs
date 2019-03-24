a8aa68bbaf56ab79d6ca493e630712a1fa62c250
TimurSirkin <timur_sirkin@mail.ru>
2019-03-24 18:58:00 +0300

    Enemyes and base changes

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class EnemyMove : MonoBehaviour
 {
     private Transform spaceBase;
     private float speed = 2.0f;
+    public  int damage = 1;
 
     // Start is called before the first frame update
     void Start()
     {
+        Console.Write("Trigerred!");
         spaceBase = GameObject.FindWithTag("Base").transform;
-
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Started");
+    }
+
+    /// <summary>
+    /// Всего хитпоинтов
+    /// </summary>
+    public int hp;
+
+    /// <summary>
+    /// Наносим урон и проверяем должен ли объект быть уничтожен
+    /// </summary>
+    /// <param name="damageCount"></param>
+
+    public void Damage(int damageCount)
+    {
+        hp -= damageCount;
+
+        if (hp <= 0)
+        {
+            // Смерть!
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        print("Trigger");
+        // Это выстрел?
+        EnemyMove enemy = otherCollider.gameObject.GetComponent<EnemyMove>();
+        if (enemy != null)
+        {
+            print("Attacked");
+            Damage(enemy.damage);
+            Destroy(enemy.gameObject);
+        }
+    }
+}
