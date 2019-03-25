3942ec939f56be901fdd1132db73f0c225390f37
Artemii Morozov <33790882+morozzArt@users.noreply.github.com>
2019-03-25 22:08:18 +0300

    change live time shot

--- Assets/Scripts/ShotScript.cs
+++ Assets/Scripts/ShotScript.cs
@@ -9,7 +9,7 @@ public class ShotScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 10);
+        Destroy(gameObject, 3);
     }
 
     // Update is called once per frame
