d6b36845158d7f60dbd9855385fa5b30e868892a
TimurSirkin <timur_sirkin@mail.ru>
2019-03-28 16:02:53 +0300

    Add Enemy3Script

---
+++ Assets/Scripts/Enemy3Script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy3Script : MonoBehaviour
+{
+    public GameObject enemy;
+    public float spawnCooldown = 2.0f;
+    private float currentCooldown;
+    
+    void Start()
+    {
+        currentCooldown = spawnCooldown;
+    }
+    
+    void Update()
+    {
+        currentCooldown -= Time.deltaTime;
+        if (currentCooldown <= 0)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemy, transform.position, Quaternion.identity);
+        currentCooldown = spawnCooldown;
+    }
+}
