d76557801427a0438fc052ff380a969b1dc18deb
varlogen <martynovpavel8@ya.ru>
2019-03-28 17:12:28 +0300

    bonus spawn

---
+++ Assets/Scripts/BonusSpawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusSpawn : MonoBehaviour
+{
+    public GameObject[] bonuses;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnBonus();
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void spawnBonus()
+    {
+        StartCoroutine(addBonus());
+    }
+
+    IEnumerator addBonus()
+    {
+        yield return new WaitForSeconds(1.0f);
+        GameObject cr_bonus = bonuses[Random.Range(0, bonuses.Length)];
+        float randX = Random.Range(-GetComponent<BoxCollider2D>().bounds.size.x / 2, GetComponent<BoxCollider2D>().bounds.size.x / 2);
+        float randY = Random.Range(-GetComponent<BoxCollider2D>().bounds.size.y / 2, GetComponent<BoxCollider2D>().bounds.size.y / 2);
+        Vector2 bonusPosition = new Vector2(randX, randY);
+        Instantiate(cr_bonus, bonusPosition, Quaternion.identity);
+    }
+}
